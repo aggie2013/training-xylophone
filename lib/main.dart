3ceed8dae43efd23b8be4bb00510fc1d7a7b1a50
Kai He <hekai_aggie2013@163.com>
2024-09-13 14:18:15 +0800

    complete project. add functions to build similar widgets

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,40 @@ import 'package:audioplayers/audioplayers.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int idx) {
+    final player = AudioPlayer();
+    player.play(AssetSource('note$idx.wav'));
+  }
+
+  Expanded buildButton(int idx, Color color) {
+    return Expanded(
+      child: TextButton(
+        onPressed: () {playSound(idx);},
+        child: Text(''),
+        style: ButtonStyle(
+        backgroundColor: WidgetStatePropertyAll<Color>(color),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: () {
-                final player = AudioPlayer();
-                player.play(AssetSource('note1.wav'));
-              },
-              child: Text('click me'),
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildButton(1,Colors.red),
+              buildButton(2,Colors.orange),
+              buildButton(3,Colors.yellow),
+              buildButton(4,Colors.green),
+              buildButton(5,Colors.teal),
+              buildButton(6,Colors.blue),
+              buildButton(7,Colors.purple),
+            ],
           ),
         ),
       ),
